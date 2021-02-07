2d98260a506fb42d2482d4f7f1efe52eb7ed3df2
RogueSensei <chris.evans9417@protonmail.com>
2021-02-07 12:33:18 +0000

    Added clog_assert function

--- include/clogger/clog.h
+++ include/clogger/clog.h
@@ -19,6 +19,9 @@ CLOGGER_API void clog_trace(const char* function_name, const char* file_name, in
 #define CLOGGER_TRACE() clog_trace(__FUNCTION__, __FILE__, __LINE__)
 #define CLOGGER_TRACE_LINE(line) clog_trace(__FUNCTION__, __FILE__, line)
 
+// Assert function
+CLOGGER_API void clog_assert(int condition, const char* location, const char* message, ...);
+
 // Log message to file: returns CLOGGER_FALSE if the file is not found etc. else CLOGGER_TRUE
 CLOGGER_API clog_bool clog_to_file(const char* file_path, const char* location, const char* message);
 
