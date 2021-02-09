a57e43187e2531d9668ac6a2eda09908da3d73cd
RogueSensei <chris.evans9417@protonmail.com>
2021-02-09 18:12:21 +0000

    Simplified build

--- tests/unit-test/expected_behaviours.h
+++
@@ -1,6 +0,0 @@
-#ifndef CLOGGER_EXPECTED_BEHAVIOURS_H
-#define CLOGGER_EXPECTED_BEHAVIOURS_H
-
-int test_clog_message_expected(const char* location, const char* format, ...);
-
-#endif //CLOGGER_EXPECTED_BEHAVIOURS_H
