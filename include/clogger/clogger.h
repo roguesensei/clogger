a913c871d1274c35e975411067a3a383f682767d
RogueSensei <chris.evans9417@protonmail.com>
2021-03-23 20:46:50 +0000

    Removed extern macro, added non-fatal assertion

--- include/clogger/clogger.h
+++ include/clogger/clogger.h
@@ -4,23 +4,31 @@
 #include "core.h"
 #include "console.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <pthread.h>
 
 // Simplified creation method
-CLOGGER_API clogger make_clogger(const char* clogger_name);
+clogger make_clogger(const char* clogger_name);
 
 // Sync
-CLOGGER_API void clogger_info(clogger* logger, const char* location, const char* message, ...);
-CLOGGER_API void clogger_debug(clogger* logger, const char* location, const char* message, ...);
-CLOGGER_API void clogger_warning(clogger* logger, const char* location, const char* message, ...);
-CLOGGER_API void clogger_error(clogger* logger, const char* location, const char* message, ...);
-CLOGGER_API void clogger_critical(clogger* logger, const char* location, const char* message, ...);
+void clogger_info(clogger* logger, const char* location, const char* message, ...);
+void clogger_debug(clogger* logger, const char* location, const char* message, ...);
+void clogger_warning(clogger* logger, const char* location, const char* message, ...);
+void clogger_error(clogger* logger, const char* location, const char* message, ...);
+void clogger_critical(clogger* logger, const char* location, const char* message, ...);
 
 // Async
-CLOGGER_API pthread_t clogger_info_async(clogger* logger, const char* location, const char* message, ...);
-CLOGGER_API pthread_t clogger_debug_async(clogger* logger, const char* location, const char* message, ...);
-CLOGGER_API pthread_t clogger_warning_async(clogger* logger, const char* location, const char* message, ...);
-CLOGGER_API pthread_t clogger_error_async(clogger* logger, const char* location, const char* message, ...);
-CLOGGER_API pthread_t clogger_critical_async(clogger* logger, const char* location, const char* message, ...);
+pthread_t clogger_info_async(clogger* logger, const char* location, const char* message, ...);
+pthread_t clogger_debug_async(clogger* logger, const char* location, const char* message, ...);
+pthread_t clogger_warning_async(clogger* logger, const char* location, const char* message, ...);
+pthread_t clogger_error_async(clogger* logger, const char* location, const char* message, ...);
+pthread_t clogger_critical_async(clogger* logger, const char* location, const char* message, ...);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //CLOGGER_CLOGGER_H
