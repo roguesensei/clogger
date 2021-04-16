524e9ae9d239148cc2e889e2080ba8b756b6de5f
RogueSensei <chris.evans9417@protonmail.com>
2021-04-16 18:02:32 +0100

    More asserts, more docs, changed naming convention for assert functions

--- src/clogger/clog_assert.c
+++ src/clogger/clog_assert.c
@@ -33,7 +33,7 @@ void clog_assert(int condition, const char* location, const char* message, ...)
     }
 }
 
-void clog_assert_equal_int32(int expected, int actual, const char* location, const char* message, ...)
+void clog_assert_int16_eq(short expected, short actual, const char* location, const char* message, ...)
 {
     va_list args;
 
@@ -59,7 +59,33 @@ void clog_assert_equal_int32(int expected, int actual, const char* location, con
     }
 }
 
-void clog_assert_equal_uint32(unsigned int expected, unsigned int actual, const char* location, const char* message, ...)
+void clog_assert_int16_neq(short not_expected, short actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, not_expected != actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> NOT %d\n", not_expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %d\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_uint16_eq(unsigned short expected, unsigned short actual, const char* location, const char* message, ...)
 {
     va_list args;
 
@@ -85,7 +111,33 @@ void clog_assert_equal_uint32(unsigned int expected, unsigned int actual, const 
     }
 }
 
-void clog_assert_equal_size(size_t expected, size_t actual, const char* location, const char* message, ...)
+void clog_assert_uint16_neq(unsigned short not_expected, unsigned short actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, not_expected != actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> NOT %u\n", not_expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %u\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_int32_eq(int expected, int actual, const char* location, const char* message, ...)
 {
     va_list args;
 
@@ -111,7 +163,241 @@ void clog_assert_equal_size(size_t expected, size_t actual, const char* location
     }
 }
 
-void clog_assert_equal_str(const char* expected, size_t expected_size, const char* actual, size_t actual_size, const char* location, const char* message, ...)
+void clog_assert_int32_neq(int not_expected, int actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, not_expected != actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> NOT %d\n", not_expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %d\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_uint32_eq(unsigned int expected, unsigned int actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, expected == actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %u\n", expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %u\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_uint32_neq(unsigned int not_expected, unsigned int actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, not_expected != actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> NOT %u\n", not_expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %u\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_int64_eq(long expected, long actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, expected == actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %ld\n", expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %ld\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_int64_neq(long not_expected, long actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, not_expected != actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> NOT %ld\n", not_expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %ld\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_uint64_eq(unsigned long expected, unsigned long actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, expected == actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %lu\n", expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %lu\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_uint64_neq(unsigned long not_expected, unsigned long actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, not_expected != actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> NOT %lu\n", not_expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %lu\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_size_eq(size_t expected, size_t actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, expected == actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %d\n", expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %d\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_size_neq(size_t not_expected, size_t actual, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, not_expected != actual, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> NOT %d\n", not_expected);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %d\n", actual);
+
+        abort();
+    }
+}
+
+void clog_assert_str_eq(const char* expected, size_t expected_size, const char* actual, size_t actual_size, const char* location, const char* message, ...)
 {
     va_list args;
 
@@ -137,6 +423,32 @@ void clog_assert_equal_str(const char* expected, size_t expected_size, const cha
     }
 }
 
+void clog_assert_str_neq(const char* not_expected, size_t expected_size, const char* actual, size_t actual_size, const char* location, const char* message, ...)
+{
+    va_list args;
+
+    va_start(args, message);
+    int result = evaluate_assert(FATAL, strncmp(not_expected, actual, expected_size) != 0, location, message, args);
+    va_end(args);
+
+    if (!result)
+    {
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[EXPECTED RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> NOT %s (%d bytes)\n", not_expected, expected_size);
+
+        clog_set_console_colour((clog_console_colour_t) {RED, CLEAR}, CLOGGER_FOREGROUND_INTENSE);
+        printf("[ACTUAL RESULT]");
+        clog_reset_console_colour();
+
+        printf(" >> %s (%d bytes)\n", actual, actual_size);
+
+        abort();
+    }
+}
+
 int clog_expect(int condition, const char* location, const char* message, ...)
 {
     va_list args;
@@ -148,7 +460,7 @@ int clog_expect(int condition, const char* location, const char* message, ...)
     return condition;
 }
 
-int clog_expect_equal_int32(int expected, int actual, const char* location, const char* message, ...)
+int clog_expect_int32_eq(int expected, int actual, const char* location, const char* message, ...)
 {
     int condition = expected == actual;
     va_list args;
--- src/clogger/clog_assert.h
+++ src/clogger/clog_assert.h
@@ -7,18 +7,147 @@
 #include <stddef.h>
 
 /// @brief Assertion function that terminates the program on failure
+/// @note For the non-fatal variant, see `clog_expect()` and the family of `expect` functions
 /// @param condition [in] Assertion to be made
 /// @param location [in] Location of the log
 /// @param message [in] Format-able string message as you would use `printf()`
 /// @param ... [in] Variable-length args
 void clog_assert(int condition, const char* location, const char* message, ...);
 
-void clog_assert_equal_int32(int expected, int actual, const char* location, const char* message, ...);
-void clog_assert_equal_uint32(unsigned int expected, unsigned int actual, const char* location, const char* message, ...);
-void clog_assert_equal_size(size_t expected, size_t actual, const char* location, const char* message, ...);
-void clog_assert_equal_str(const char* expected, size_t expected_size, const char* actual, size_t actual_size, const char* location, const char* message, ...);
+/// @brief Make an equality assertion of two 16-bit signed integers
+/// @param expected [in] The expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_int16_eq(short expected, short actual, const char* location, const char* message, ...);
+
+/// @brief Make an inequality assertion of two 16-bit signed integers
+/// @param not_expected [in] The not expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_int16_neq(short not_expected, short actual, const char* location, const char* message, ...);
+
+/// @brief Make an equality assertion of two 16-bit unsigned integers
+/// @param expected [in] The expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_uint16_eq(unsigned short expected, unsigned short actual, const char* location, const char* message, ...);
+
+/// @brief Make an inequality assertion of two 16-bit unsigned integers
+/// @param not_expected [in] The not expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_uint16_neq(unsigned short not_expected, unsigned short actual, const char* location, const char* message, ...);
+
+/// @brief Make an equality assertion of two 32-bit signed integers
+/// @param expected [in] The expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_int32_eq(int expected, int actual, const char* location, const char* message, ...);
+
+/// @brief Make an inequality assertion of two 32-bit signed integers
+/// @param not_expected [in] The not expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_int32_neq(int not_expected, int actual, const char* location, const char* message, ...);
+
+/// @brief Make an equality assertion of two 32-bit unsigned integers
+/// @param expected [in] The expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_uint32_eq(unsigned int expected, unsigned int actual, const char* location, const char* message, ...);
+
+/// @brief Make an inequality assertion of two 32-bit unsigned integers
+/// @param not_expected [in] The not expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_uint32_neq(unsigned int not_expected, unsigned int actual, const char* location, const char* message, ...);
+
+/// @brief Make an equality assertion of two 64-bit signed integers
+/// @param expected [in] The expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_int64_eq(long expected, long actual, const char* location, const char* message, ...);
+
+/// @brief Make an inequality assertion of two 64-bit signed integers
+/// @param not_expected [in] The not expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_int64_neq(long not_expected, long actual, const char* location, const char* message, ...);
+
+/// @brief Make an equality assertion of two 64-bit unsigned integers
+/// @param expected [in] The expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_uint64_eq(unsigned long expected, unsigned long actual, const char* location, const char* message, ...);
+
+/// @brief Make an inequality assertion of two 64-bit unsigned integers
+/// @param not_expected [in] The not expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_uint64_neq(unsigned long not_expected, unsigned long actual, const char* location, const char* message, ...);
+
+/// @brief Make an equality assertion of two `size_t` values
+/// @param expected [in] The expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_size_eq(size_t expected, size_t actual, const char* location, const char* message, ...);
+
+/// @brief Make an inequality assertion of two `size_t` values
+/// @param not_expected [in] The not expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_size_neq(size_t not_expected, size_t actual, const char* location, const char* message, ...);
+
+/// @brief Make an equality assertion of two strings
+/// @param expected [in] The expected value
+/// @param expected_size [in] The `sizeof` the expected string
+/// @param actual [in] The actual value
+/// @param actual_size [in] The `sizeof` the actual string
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_str_eq(const char* expected, size_t expected_size, const char* actual, size_t actual_size, const char* location, const char* message, ...);
+
+/// @brief Make an inequality assertion of two strings
+/// @param not_expected [in] The not expected value
+/// @param not_expected_size [in] The `sizeof` the not expected string
+/// @param actual [in] The actual value
+/// @param actual_size [in] The `sizeof` the actual string
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+void clog_assert_str_neq(const char* not_expected, size_t not_expected_size, const char* actual, size_t actual_size, const char* location, const char* message, ...);
 
 /// @brief Assertion function that does not terminate the program on failure
+/// @note For the fatal variant, see `clog_assert()` and the family of `assert` functions
 /// @param condition [in] Assertion to be made
 /// @param location [in] Location of the log
 /// @param message [in] Format-able string message as you would use `printf()`
@@ -26,6 +155,13 @@ void clog_assert_equal_str(const char* expected, size_t expected_size, const cha
 /// @return Assertion result
 int clog_expect(int condition, const char* location, const char* message, ...);
 
-int clog_expect_equal_int32(int expected, int actual, const char* location, const char* message, ...);
+/// @brief Make an equality non-fatal assertion of two 32-bit signed integers
+/// @param expected [in] The expected value
+/// @param actual [in] The actual value
+/// @param location [in] Location of the log
+/// @param message [in] Format-able string message as you would use `printf()`
+/// @param ... [in] Variable-length args
+/// @return Assertion result
+int clog_expect_int32_eq(int expected, int actual, const char* location, const char* message, ...);
 
 #endif //CLOGGER_CLOG_ASSERT_H
