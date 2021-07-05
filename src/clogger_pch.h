efe8fdff991eafe382cfb70b623ee626d8211f5e
RogueSensei <chris.evans9417@protonmail.com>
2021-07-05 20:55:03 +0100

    Moved header, cleaner source build

--- src/clogger_pch.h
+++ src/clogger_pch.h
@@ -1,11 +1,25 @@
 #ifndef CLOGGER_CLOGGER_PCH_H
 #define CLOGGER_CLOGGER_PCH_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef WIN32
+#ifndef _TIMESPEC_DEFINED
+#define _TIMESPEC_DEFINED
+#endif
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
 #include <pthread.h>
 #include <time.h>
+
+#ifdef __cplusplus
+	}
+#endif
 
 #endif //CLOGGER_CLOGGER_PCH_H
